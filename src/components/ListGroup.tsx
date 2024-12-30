f88ed452b01c8752eb95bbc25cae7307826a78dc
Huu Thang Tran <thanghuu.work@gmail.com>
2024-12-29 20:40:40 -0500

    Adding event handler

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,22 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  let items = ["New York", "San Francisco", "TokyO", "London", "Paris"];
-  items = [];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // items = [];
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
 
   return (
     <>
       <h1>List</h1>
+      {/*true && 1 =>  1, true && "Thang" => Thang*/}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.length === 0 && <p>No item found</p>}
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
