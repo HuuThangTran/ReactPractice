ec4289f592c83822d5ca0d80335b1788757c3208
Huu Thang Tran <thanghuu.work@gmail.com>
2025-01-03 21:58:51 -0500

    Styling component with vanilla CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -9,10 +10,28 @@ function App() {
     console.log(item);
   };
 
+  const [alertVisiblity, setAlertVisibility] = useState(false);
+
   return (
-    <Button color="danger" onClick={() => console.log("Clicked")}>
-      Button
-    </Button>
+    <>
+      {/* {alertVisiblity && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button
+        color="danger"
+        onClick={() => {
+          console.log("Clicked");
+          setAlertVisibility(true);
+        }}
+      >
+        Button
+      </Button> */}
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
+    </>
   );
 }
 
