a15342f46a59cda2260ac0c5f3a485b4067de094
mle288 <mle288@ucsc.edu>
2025-01-14 20:53:30 -0500

    Form Validation

--- src/App.tsx
+++ src/App.tsx
@@ -6,50 +6,42 @@ import "./App.css";
 import Like from "./components/Like";
 import Cart from "./components/Cart";
 import Navbar from "./components/Navbar";
-
+import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 function App() {
   //==========================================
   // sharing State between Component exercise
-  const [cartItem, setCartItem] = useState(["Product 1", "Product 2"]);
+  // const [cartItem, setCartItem] = useState(["Product 1", "Product 2"]);
   //==========================================
 
+  //==========================================
+  // Updating state
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    item: [
+      { id: 1, title: "Prodcut 1", quantity: 1 },
+      { id: 2, title: "Prodcut 2", quantity: 1 },
+    ],
+  });
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      item: cart.item.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
+    });
+  };
+
+  //==========================================
   // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   // const handleSelectItem = (item: string) => {
   //   console.log(item);
   // };
 
-  const [alertVisiblity, setAlertVisibility] = useState(false);
+  // const [alertVisiblity, setAlertVisibility] = useState(false);
 
-  return (
-    <>
-      {/* ========================================== */}
-      {/* // sharing State between Component exercise const [cartItem, setCartItem] */}
-      <Navbar itemCartCount={cartItem.length} />
-      <Cart cartItemName={cartItem} onClick={() => setCartItem([])} />
-      {/* ========================================== */}
-
-      {/* {alertVisiblity && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button
-        color="danger"
-        onClick={() => {
-          console.log("Clicked");
-          setAlertVisibility(true);
-        }}
-      >
-        Button
-      </Button> */}
-      {/* <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup> */}
-      {/* <Button onClick={() => console.log("Clicked")}>My Button</Button> */}
-      {/* <Like onClick={() => console.log("Clicked")}></Like> */}
-    </>
-  );
+  return <Form></Form>;
 }
 
 export default App;
