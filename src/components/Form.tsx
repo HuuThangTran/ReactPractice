1a251d22f88fb72f377149c1c3f7e00dc2ab9d6e
Thang <thanghuu.work@gmail.com>
2025-01-15 23:49:30 -0500

    Updating README.md

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -20,7 +20,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
   //   console.log(register);
   //   const nameRef = useRef<HTMLInputElement>(null);
@@ -59,7 +59,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger">{errors.age.message}.</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
